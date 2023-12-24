e83385b16ece64b312c47fa787ffa60f6bd71c3e
Yugoff <vv.yugoff@gmail.com>
2023-12-24 17:22:41 +0500

    Update stream.py

--- stream.py
+++ stream.py
@@ -1,34 +1,14 @@
-# import cv2
-
-# usr = "admin"
-# pwd = "Vfksi13*"
-# ip = "192.168.0.3" # default for access-point mode
-# cap = cv2.VideoCapture(f"rtsp://{usr}:{pwd}@{ip}/camera-test")
-
-# while(cap.isOpened()):
-#     ret, frame = cap.read()
-#     cv2.imshow('frame', frame)
-#     if cv2.waitKey(20) & 0xFF == ord('q'):
-#         break
-# cap.release()
-# cv2.destroyAllWindows()
-
 import cv2
 
 usr = "admin"
-pwd = "Vfksi13*"
-ip = "192.168.0.3"
+pwd = "[password]"
+ip = "192.168.0.1" # default for access-point mode
+cap = cv2.VideoCapture(f"rtsp://{usr}:{pwd}@{ip}/camera-test")
 
-cap = cv2.VideoCapture(f"rtsp://{usr}:{pwd}@{ip}/live/ch00_1", cv2.CAP_FFMPEG)
-cap.set(cv2.CAP_PROP_FOURCC, cv2.VideoWriter_fourcc(*"MJPG"))
-
-while cap.isOpened():
+while(cap.isOpened()):
     ret, frame = cap.read()
-    if not ret:
-        break
-    cv2.imshow("frame", frame)
+    cv2.imshow('frame', frame)
     if cv2.waitKey(20) & 0xFF == ord('q'):
         break
-
 cap.release()
 cv2.destroyAllWindows()
